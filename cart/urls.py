b5206d72a27b0fd2a46b47395aa66385e49a812b
Adebobola <adepojuadebobola6@gmail.com>
2024-12-09 16:04:19 +0100

    confused!! need a nap.

--- cart/urls.py
+++ cart/urls.py
@@ -3,5 +3,6 @@ from .import views
 
 
 urlpatterns = [
-    path('cart/', views.cart_detail)
+    path('cart/', views.cart_detail_get),
+    path('cart/checkout/', views.cart_checkout)
 ]
--- cart/views.py
+++ cart/views.py
@@ -5,14 +5,33 @@ from rest_framework.authentication import SessionAuthentication, BasicAuthentica
 from rest_framework.permissions import IsAuthenticated
 from .models import Cart
 from .serializer import CartSerializer
+from django.http import JsonResponse
 
 
-@api_view(['GET', 'POST'])
+@api_view(['GET', 'POST', 'PUT'])
 @authentication_classes([SessionAuthentication, BasicAuthentication])
 @permission_classes([IsAuthenticated])
-def cart_detail(request):
+def cart_detail_get(request):
     if request.method == "GET":
         cart = Cart.objects.get(user=request.user)
         cart_serializer = CartSerializer(cart)
 
         return Response(cart_serializer.data)
+
+    dish_id = request.data.get("dish_id")
+    if request.method == "PUT":
+        cart = Cart.objects.get(user=request.user)
+        about_to = cart.dishes.get(id=dish_id)
+        if about_to is not None:
+
+            cart.dishes.remove(about_to)
+            cart_serializer = CartSerializer(cart)
+            return Response(cart)
+        return Response({"message": "Dish no longer in order"})
+
+
+@api_view(['GET', 'POST'])
+@authentication_classes([SessionAuthentication, BasicAuthentication])
+@permission_classes([IsAuthenticated])
+def cart_checkout(request):
+    pass
